511af405017a97932f699d93be5c2d263a8c8e5d
Dimitri Igoshev <dimi.igoshev@gmail.com>
2026-07-21 19:30:53 +0300

    Update estimate-pdf.service.ts

--- backend/src/estimate/estimate-pdf.service.ts
+++ backend/src/estimate/estimate-pdf.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import * as PdfPrinter from 'pdfmake';
+const PdfPrinter = require('pdfmake/src/printer');
 import type { TDocumentDefinitions, Content } from 'pdfmake/interfaces';
 import { Estimate, EstimateDocument } from './schemas/estimate.schema';
 import { ProjectsService } from '../projects/projects.service';
